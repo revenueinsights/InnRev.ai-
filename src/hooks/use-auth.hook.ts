38832de4c309fd645edee380a4453021df73ab03
Gustavo <gustavoaugustocar@hotmail.com>
2024-03-26 18:59:16 -0300

    Chore: auth implementation init

--- src/hooks/use-auth.hook.ts
+++ src/hooks/use-auth.hook.ts
@@ -5,10 +5,10 @@ import { useQuery } from '@tanstack/react-query';
 import { auth } from '@/lib/auth.lib';
 
 export function useAuth() {
-  const { data, isLoading } = useQuery({
+  const { data } = useQuery({
     queryKey: ['auth'],
     queryFn: async () => auth(),
   });
 
-  return { authData: data, isLoading };
+  return { authData: data };
 }
